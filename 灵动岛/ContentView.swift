c55e242f957c95ce0edac161855e8918063732ad
雪穗子 <1340272051@qq.com>
2026-07-01 16:54:06 +0800

    Initial Commit

---
+++ 灵动岛/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  灵动岛
+//
+//  Created by 雪穗子 on 1/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
